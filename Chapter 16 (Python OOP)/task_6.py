110f52299e6ed1e0c01e9f7df63a6f3ba281b62a
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-21 23:46:54 +0100

    task6 is completed

--- Chapter 16 (Python OOP)/task_6.py
+++ Chapter 16 (Python OOP)/task_6.py
@@ -10,3 +10,16 @@ Example Output:
 
 The Great Gatsby by F. Scott Fitzgerald (Published: 1925)
 '''
+
+class Book:
+    def __init__(self, title, author, year_published):
+        self.title = title
+        self.author = author
+        self.year_published = year_published
+
+    def __str__(self):
+        return f"{self.title} by {self.author} (Published: {self.year_published})"
+    
+my_book = Book("The Great Gatsby", "Scott Fitzgerald", 1925)
+
+print(my_book)
