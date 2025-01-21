b3588e03bcadd084321e5933e67c0aa0922cd885
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-21 23:14:18 +0100

    task4 is completed

--- Chapter 16 (Python OOP)/task_4.py
+++ Chapter 16 (Python OOP)/task_4.py
@@ -12,3 +12,17 @@ Model: Model S
 Year: 2022
 Battery Size: 100
 '''
+class Vehicle:
+    def __init__(self, make: str, model: str, year: int):
+        self.make = make
+        self.model = model
+        self.year = year
+    
+class ElectricCar(Vehicle):
+    def __init__(self, make, model, year, battery_size: int):
+        super().__init__(make, model, year)
+        self.battery_size = battery_size
+
+my_car = ElectricCar("Tesla", "Model S", 2022, 100)
+
+print(f"Make: {my_car.make}\nModel: {my_car.model}\nYear: {my_car.year}\nBattery Size: {my_car.battery_size}")
