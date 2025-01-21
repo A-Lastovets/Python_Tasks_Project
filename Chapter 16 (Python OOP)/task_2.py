fd4e87a9cb01717f20b7a72ce6bfee3eab6e02d8
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-21 22:17:40 +0100

    task2 is completed

--- Chapter 16 (Python OOP)/task_2.py
+++ Chapter 16 (Python OOP)/task_2.py
@@ -13,3 +13,10 @@ Width: 5
 Height: 10
 
 '''
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+instance = Rectangle(5, 10)
+print(f"Width: {instance.width}\nHeight: {instance.height}")
