b3b910b3788fa1b9945090233cd2280e226ae085
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-22 21:17:36 +0100

    task8 is completed

--- Chapter 16 (Python OOP)/task_8.py
+++ Chapter 16 (Python OOP)/task_8.py
@@ -13,3 +13,24 @@ Example Output:
 Employee name: Alice, Salary: 120000
 Is the salary high? True
 '''
+
+class Employee:
+    def __init__(self, name: str, salary: float):
+        self.name = name
+        self.salary = salary
+
+    @classmethod
+    def from_string(cls, emp_info: str):
+        name, salary = emp_info.split(", ")
+        return cls(name, float(salary))
+    
+    @staticmethod
+    def is_high_salary(salary: float):
+        return salary > 100000
+    
+emp_info = "Alice, 120000"
+employee = Employee.from_string(emp_info)
+
+print(f"Employee Name: {employee.name}, Salary: {employee.salary}")
+
+print("Is the salary high?", Employee.is_high_salary(employee.salary))
