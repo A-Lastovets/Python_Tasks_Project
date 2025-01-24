75fc2ff90ae7aaf479b72b90af75f79b7eab94f5
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-24 21:52:02 +0100

    task3 is completed

--- Chapter 19 (Python higher-order functions)/task_3.py
+++ Chapter 19 (Python higher-order functions)/task_3.py
@@ -8,4 +8,14 @@ Example Output:
 HELLO
 
 '''
+def uppercase_decorator(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+    return wrapper
 
+@uppercase_decorator
+def greet():
+    return "Hello"
+
+print(greet())
