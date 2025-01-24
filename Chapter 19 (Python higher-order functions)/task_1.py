52e610e72bbcd983c0fe245acb6a4c1cb02e8648
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-24 20:13:39 +0100

    start home work

---
+++ Chapter 19 (Python higher-order functions)/task_1.py
@@ -0,0 +1,11 @@
+'''
+Task 1: Basic Higher-Order Function
+Problem:
+Create a higher-order function apply_function(func, value) that:
+Takes a function func and a value value as arguments.
+Applies the function func to the value value and returns the result.
+Test apply_function by passing in a simple function like lambda x: x * 3 and a value like 4.
+
+Example Output:
+12
+'''
