d592392097e59c786321bd755db39a4f06a37c8f
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-27 17:44:57 +0100

    starting homework

---
+++ Chapter 24 (Python files management)/task_1.py
@@ -0,0 +1,18 @@
+'''
+Task 1: Write User Input to a File
+Problem:
+Create a program that prompts the user to enter their name, age, and favorite color. Write this information to a file called user_info.txt in the following format:
+Name: [User's Name]  
+Age: [User's Age]  
+Favorite Color: [User's Favorite Color]
+
+Example Input:
+Enter your name: Alice  
+Enter your age: 25  
+Enter your favorite color: Blue
+
+Expected Output in user_info.txt:
+Name: Alice  
+Age: 25  
+Favorite Color: Blue
+'''
---
+++ Chapter 24 (Python files management)/task_3.py
@@ -0,0 +1,20 @@
+'''
+Task 3: Append and Display File Content
+Problem:
+Create a program that appends a new line to a file called log.txt with the current date and time. Then, read and display the entire file content.
+If the file doesn't exist, create it.
+Append the current date and time in the format: YYYY-MM-DD HH:MM:SS.
+Print the entire content of the file after the new line is added.
+Example Execution:
+On the first run:
+log.txt created.
+Appended: 2024-12-26 15:00:00
+
+On the second run:
+Appended: 2024-12-26 15:01:00
+
+File content:
+2024-12-26 15:00:00
+2024-12-26 15:01:00
+
+'''
