d75894a43c1d2485101652f52835cede9eb9a685
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2025-01-28 20:59:44 +0100

    task3 is completed

--- Chapter 24 (Python files management)/task_3.py
+++ Chapter 24 (Python files management)/task_3.py
@@ -18,3 +18,25 @@ File content:
 2024-12-26 15:01:00
 
 '''
+from datetime import datetime
+import os
+
+def append_data():
+    file_name = "./Chapter 24 (Python files management)/log.txt"
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file_exists = os.path.exists(file_name)
+
+    try:
+        with open(file_name, "a") as file:
+            file.write(current_time + "\n")
+            if not file_exists:
+                print("log.txt created.")
+            print(f"Appended: {current_time}")
+
+        with open(file_name, "r") as file:
+            print("File content:")
+            print(file.read())
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+append_data()
